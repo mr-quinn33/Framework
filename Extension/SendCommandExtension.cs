d09a238b811bf845f3ec1bf95c4b0d77b2232d59
Quinn <541867284@qq.com>
2022-05-31 19:20:03 +0800

    Create SendCommandExtension.cs

---
+++ Extension/SendCommandExtension.cs
@@ -0,0 +1,18 @@
+﻿using Framework.Interface;
+using Framework.Interface.Access;
+
+namespace Framework.Extension
+{
+    public static class SendCommandExtension
+    {
+        public static void SendCommand(this ISendCommand self, ICommand command)
+        {
+            self.GetArchitecture().SendCommand(command);
+        }
+
+        public static void SendCommand<T>(this ISendCommand self) where T : ICommand, new()
+        {
+            self.GetArchitecture().SendCommand<T>();
+        }
+    }
+}
