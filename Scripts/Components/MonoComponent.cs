de260a53d4dcabd8b70bfc67a272a0079950c3d4
mr-quinn33 <541867284@qq.com>
2022-10-16 16:20:16 +0800

    Create MonoComponent.cs

---
+++ Scripts/Components/MonoComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Framework.Components;
+using Framework.GameModes;
+using Framework.Rules;
+using UnityEngine;
+
+namespace Framework.Scripts.Components
+{
+    public abstract class MonoComponent<T> : MonoBehaviour, IComponent where T : GameMode<T>, new()
+    {
+        IGameMode IGetGameMode.GetGameMode()
+        {
+            return typeof(T).GetMethod("Load", BindingFlags.FlattenHierarchy | BindingFlags.Public | BindingFlags.Static)?.Invoke(null, null) as IGameMode;
+        }
+    }
+}
