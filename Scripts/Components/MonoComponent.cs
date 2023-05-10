5ff34dff6e5f27ee3b9934c929a8375f7869b08f
Quinn Mr <541867284@qq.com>
2023-05-10 16:17:56 +0800

    Delete generic IComponent

--- Scripts/Components/MonoComponent.cs
+++ Scripts/Components/MonoComponent.cs
@@ -1,10 +1,12 @@
 ﻿using Framework.Components;
 using Framework.GameModes;
+using Framework.Rules;
 using UnityEngine;
 
 namespace Framework.Scripts.Components
 {
-    public abstract class MonoComponent<T> : MonoBehaviour, IComponent<T> where T : GameMode<T>, new()
+    public abstract class MonoComponent<T> : MonoBehaviour, IComponent where T : GameMode<T>, new()
     {
+        IGameMode IGetGameMode.GetGameMode() => GameMode<T>.Load();
     }
 }
