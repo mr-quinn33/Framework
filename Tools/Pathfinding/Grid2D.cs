be9aed0475fa3b3052b05e9ff0d10649820fefe6
Quinn Mr <541867284@qq.com>
2023-05-21 13:19:05 +0800

    Update Grid2D.cs

--- Tools/Pathfinding/Grid2D.cs
+++ Tools/Pathfinding/Grid2D.cs
@@ -11,7 +11,7 @@ namespace Framework.Tools.Pathfinding
 
         T this[int x, int y] { get; }
 
-        T this[Vector2Int pos] { get; }
+        T this[Vector2Int coordinate] { get; }
 
         bool IsWithinBounds(int x, int y);
 
@@ -21,7 +21,7 @@ namespace Framework.Tools.Pathfinding
 
         bool Contains(T t, out int x, out int y);
 
-        bool Contains(T t, out Vector2Int position);
+        bool Contains(T t, out Vector2Int coordinate);
 
         IReadOnlyList<T> GetNeighbours(int x, int y, bool includeDiagonals);
 
@@ -131,10 +131,10 @@ namespace Framework.Tools.Pathfinding
             return false;
         }
 
-        public bool Contains(T t, out Vector2Int position)
+        public bool Contains(T t, out Vector2Int coordinate)
         {
             var result = Contains(t, out var x, out var y);
-            position = new Vector2Int(x, y);
+            coordinate = new Vector2Int(x, y);
             return result;
         }
 
